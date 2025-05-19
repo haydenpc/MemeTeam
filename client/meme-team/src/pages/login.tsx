c6482d96b4a73827b941f00ae49a2ea82fa8c041
Hayden <haydenpc369@gmail.com>
2025-05-19 11:29:17 -0500

    updated login

--- client/meme-team/src/pages/login.tsx
+++ client/meme-team/src/pages/login.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
- 
+import AuthService from "../services/auth.service.js" 
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -10,7 +10,6 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "@/components/ui/form"
 import {
     Card,
@@ -23,11 +22,11 @@ import {
 import { Input } from "@/components/ui/input"
  
 const formSchema = z.object({
-  username: z.string().min(10, {
-    message: "Username must be at least 10 characters.",
+  username: z.string().min(4, {
+    message: "Username must be at least 4 characters.",
   }),
-    password: z.string().min(10, {
-        message: "password must be at least 10 characters.",
+    password: z.string().min(4, {
+        message: "password must be at least 4 characters.",
   }),
 })
  
@@ -40,54 +39,63 @@ export default function Login() {
         },
       })
       
-      function onSubmit(values: z.infer<typeof formSchema>) {
+      async function onSubmit(values: z.infer<typeof formSchema>) { 
         console.log(values)
+        const x = await AuthService.login(values.username, values.password);
+        console.log(x)
       }
  
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-        <Card className="w-[350px]">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <CardHeader>
-                        <CardTitle>Login</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                        <>
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Username" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                            <FormMessage/>
-                                </FormItem>
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Password" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This isnt visible to other users
-                                </FormDescription>
-                            <FormMessage />
-                            </FormItem>
-                        </>
-                        )}
-                    />
-                    </CardContent>
-            <CardFooter>
-                <Button type="submit">Submit</Button>
-            </CardFooter>
-                </form>
-            </Form>
-        </Card>
-    </div>
-  )
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+          <Card className="w-[350px]">
+              <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                      <CardHeader>
+                          <CardTitle>Login</CardTitle>
+                      </CardHeader>
+                      <CardContent>
+                          <FormField
+                          control={form.control}
+                          name="username"
+                          render={({ field }) => (
+                              <FormItem className="pb-4">
+                                  <FormLabel>Username</FormLabel>
+                                  <FormControl>
+                                      <Input placeholder="Username" {...field} />
+                                  </FormControl>
+                                  <FormDescription>
+                                      This is your public display name.
+                                  </FormDescription>
+                              </FormItem>
+                          )}
+                        />
+                          <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                              <FormItem className="pb-4">
+                                  <FormLabel>Password</FormLabel>
+                                  <FormControl>
+                                      <Input type="password" placeholder="Password" {...field} />
+                                  </FormControl>
+                              </FormItem>
+                          )}
+                      />
+                      </CardContent>
+                      <CardFooter className="flex flex-col items-center gap-2">
+              <Button type="submit" className="w-full">
+                Submit
+              </Button>
+              <p className="text-sm text-muted-forground">
+                Dont have a account yet?
+              </p>
+                <a href="/signup" className="flec flex-col items-center text-primary hover:underline">
+                  Signup
+                </a>
+          </CardFooter>
+                  </form>
+              </Form>
+          </Card>
+      </div>
+    )
 }
