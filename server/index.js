1fb5c44c055094fbad4dfc9418576b4dbc289033
Hayden <haydenpc369@gmail.com>
2025-05-19 11:32:47 -0500

    added cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 var bodyParser = require('body-parser')
+const cors = require("cors")
 const app = express();
 const port = 3000;
 
 const healthRoutes = require('./routes/health');
 const userRoutes = require('./routes/user');
-app.use(bodyParser.json())
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type'],
+  credentials: true 
+}));
+
+app.use(bodyParser.json())
 
 app.use('/health', healthRoutes);
 app.use('/user', userRoutes);
